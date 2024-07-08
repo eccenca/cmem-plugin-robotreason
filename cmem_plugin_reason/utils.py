8b7f4e60ccee6a4de421fb9cbfd42793ad8d346c
muddymudskipper <thomas.wilmering@gmail.com>
2024-07-08 18:28:15 +0100

    fix jar path

--- cmem_plugin_reason/utils.py
+++ cmem_plugin_reason/utils.py
@@ -21,6 +21,8 @@ from cmem_plugin_base.dataintegration.plugins import ExecutionContext, WorkflowP
 from cmem_plugin_base.dataintegration.types import BoolParameterType, IntParameterType
 from defusedxml import minidom
 
+from . import __path__
+
 REASONERS = OrderedDict(
     {
         "elk": "ELK",
@@ -216,7 +218,7 @@ def get_provenance(plugin: WorkflowPlugin, context: ExecutionContext) -> dict | 
 
 def robot(cmd: str, max_ram_percentage: int) -> CompletedProcess:
     """Run robot.jar"""
-    jar = Path("cmem_plugin_reason") / "bin" / "robot.jar"
+    jar = Path(__path__[0]) / "bin" / "robot.jar"
     cmd = f"java -XX:MaxRAMPercentage={max_ram_percentage} -jar {jar} " + cmd
     return run(shlex.split(cmd), check=False, capture_output=True)  # noqa: S603
 
